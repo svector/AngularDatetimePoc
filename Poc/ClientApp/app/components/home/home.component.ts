48da9208607f722dca9d96c6e154b542a2979b7b
Toby Milne <tm@svector.co.uk>
2016-11-25 11:10:37 +0000

    Tweak some styles

--- Poc/ClientApp/app/components/home/home.component.ts
+++ Poc/ClientApp/app/components/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home',
-    template: require('./home.component.html')
+    template: require('./home.component.html'),
+    styles: ['/deep/ .dropdown-menu {background- color: #bbbbbb;min-width: 220px;}']
 })
 export class HomeComponent {
     value: Date;
